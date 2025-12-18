f77ee93440bf9724ed8c93edb5115e051ca80fa4
Jediael Borges <jediaelborges23@gmail.com>
2025-12-18 18:13:57 -0300

    feat: video pitch

--- src/app/pages/bfacademy/bfacademy.ts
+++ src/app/pages/bfacademy/bfacademy.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from "../../shared/header/header";
 import { Gradient } from "../../shared/gradient/gradient";
 import { NgOptimizedImage } from "@angular/common";
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-bfacademy',
@@ -9,5 +10,10 @@ import { NgOptimizedImage } from "@angular/common";
   templateUrl: './bfacademy.html',
 })
 export class Bfacademy {
-
+  private sanitizer = inject(DomSanitizer)
+  readonly videoId = signal<string>('meO3Z8a_6QQ');
+  getSafeUrl(): SafeResourceUrl {
+    const url = `https://www.youtube.com/embed/${this.videoId()}`
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
